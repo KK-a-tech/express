473554ce42c6fc9a35a68495dafb9696391c287e
KK-a-tech <kento.k711@gmail.com>
2024-11-07 01:43:10 +0900

    feat:静的ページの表示を更新

--- src/app.js
+++ src/app.js
@@ -1,23 +1,19 @@
 const express = require("express");
-
-const app = express();
 const path = require('path');
-
+const app = express();
 const userRouter = require("./routes/user");
-
 const PORT = 3000;
 
 // app.use(mylogger);
-
-// app.use(express.static("public"));
-app.set("view engine", "ejs");
-app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, "public")));
+// app.set("view engine", "ejs");
+// app.set('views', path.join(__dirname, 'views'));
 app.get("/", (req, res) => {
   // console.log("hello express");
   // res.send("<h1>こんにちは</h1>");
   // res.sendStatus(500);
   // res.status(500).send("エラーです。");
-  res.render("index", {text: "nodejsとExpress"})
+  // res.render("index", {text: "nodejsとExpress"})
 });
 
 // ルーティング
